d6242738005fc68dcab1755ad8ff8625efde5d06
Geeth Wishkamal <41125548+geethwish@users.noreply.github.com>
2024-02-25 18:26:18 +0530

    title changed

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ const IBMPlex = IBM_Plex_Sans({
 });
 
 export const metadata: Metadata = {
-  title: "Imagine Bot",
+  title: "Imaginify Warlock",
   description: "AI-powered image generator",
 };
 
